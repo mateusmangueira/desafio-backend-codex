c7707f65d6f6af5ec760518d82e3e56a48e75dbd
mateusmangueira <mateus.mangueira@ccc.ufcg.edu.br>
2021-04-04 12:04:56 -0300

    Criando e configurando o MongoDB

---
+++ src/database/index.js
@@ -0,0 +1,29 @@
+import mongoose from 'mongoose';
+
+//Colocar os Models da aplicacao nessa parte
+
+const models = []; //Adicionar o model criado nesse array
+
+class Database {
+  constructor() {
+    this.init();
+    this.mongo();
+  }
+
+  init() {
+    /*models
+      .map(model => model.init(this.connection))
+      .map(model => model.associate && model.associate(this.connection.models));
+      */
+  }
+
+  mongo() {
+    this.mongoConnection = mongoose.connect('mongodb+srv://mateus:123321@backend-desafio-codex.doriq.mongodb.net/backend-desafio?retryWrites=true&w=majority', {
+      useNewUrlParser: true,
+      useFindAndModify: true,
+      useUnifiedTopology: true,
+    });
+  }
+}
+
+export default new Database();
--- src/routes.js
+++ src/routes.js
@@ -4,7 +4,7 @@ const routes = new Router(); // Criando o roteador e associando a uma constante 
 
 //Criar rotas POST,GET,PUT ou DELETE a partir daqui. de acordo com o que precisa na aplicacao. Vou deixar 1 rota GET para testar o Server
 
-routes.get('/tasks', (req, res) => {
+routes.get('/users', (req, res) => {
   return res.json(req.body);
 }) //Depois vamos refatorar essas rotas utilizando Controllers. Cada classe do Sitema tem um Model e um Controller. Model serve para criar a classe basica de representacao da abstracao no sistema e ser persistida no BD e o Controller para fazer a logica de negocio desse Model na aplicacao.
 
