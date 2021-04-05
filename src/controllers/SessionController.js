42c1f01d0a3b075da278efdada5aaeb1149c34e6
mateusmangueira <mateus.mangueira@ccc.ufcg.edu.br>
2021-04-05 20:33:47 -0300

    Commit complementar a reuniao 1 05/04

--- src/controllers/SessionController.js
+++ src/controllers/SessionController.js
@@ -4,13 +4,7 @@ import authConfig from '../config/auth';
 
 class SessionController {
   async store(req, res) {
-    const {email, name, password } = req.body;
-       let user = await User.findOne({email})    
-        if(!user) {
-            user = await User.create({email, name, password});
-        }
-        return res.json(user);
-    }
+  }
 }
 
 export default new SessionController();
